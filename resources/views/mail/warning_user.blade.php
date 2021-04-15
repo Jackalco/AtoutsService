59ecb17c1fb1c5c7606b7d2c24dbc70edcad4611
Jackalco <vincent.jacques@viacesi.fr>
2021-04-15 15:23:22 +0200

    Add responsive to the payment view

--- resources/views/mail/warning_user.blade.php
+++ resources/views/mail/warning_user.blade.php
@@ -1,7 +1,7 @@
 <h3>Bonjour</h3> <br>
 
-Nous vous informons que votre abonnement à Atouts Services prendra fin le {{$provider->end_date}}.<br>
-Si vous souhaitez que ce prestataire {{$provider->name}} soit toujours visible sur notre site, vous devez renouveller votre abonnement.<br>
+Nous vous informons que votre abonnement à Atouts Services prendra fin le <strong>{{$provider->end_date}}</strong>.<br>
+Si vous souhaitez que ce prestataire <strong>{{$provider->name}}</strong> soit toujours visible sur notre site, vous devez renouveller votre abonnement.<br>
 Ce renouvellement s'effectue dans votre Espace membre, dans la catégorie "Voir vos prestataires".<br><br>
 
 Bonne journée,<br>
